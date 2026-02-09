51bdac0e18cf4a79ff909a6ed78ee6349c8bc9f9
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-09 17:59:53 +0100

    compimento secondo esercizio

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 let input = prompt("Inserisci la parola:");
-let poli
+
 
 
 function inversione(str) {
@@ -29,6 +29,50 @@ let messaggioPalindromo = polindromo(input, parola_rev);
 
 console.log(parola_rev)
 console.log(messaggioPalindromo)
+
+
+// secondo esercizio
+
+function numeroRandom(){
+    return Math.floor(Math.random() * 5) + 1
+}
+
+function sum(x,y){
+    return Number(x)+Number(y)
+}
+
+function pair(n){
+    if(n % 2===0){
+        return "vince il pari"
+
+    }
+    else{
+        return "vince il dispari"
+    }
+
+}
+
+
+
+let somma
+let risultato
+let numeroG1 = prompt("scegli un numero")
+console.log("il numero scelto dal giocatore uno è:", numeroG1)
+
+
+let numeroG2 = numeroRandom()
+console.log("il numero scelto da giocatore numero due è", numeroG2)
+
+somma= sum(numeroG1 , numeroG2)
+console.log("la somma dei due numeri è", somma);
+
+risultato =pair(somma)
+console.log(risultato)
+
+
+
+
+
 
 
 
