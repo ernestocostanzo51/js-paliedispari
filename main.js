8898cf67fd0a665de19da0e41bd0c15e2c412245
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-09 17:03:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+let input = prompt("Inserisci la parola:");
+let input_rev
+let poli
+
+
+function inversione(str) {
+    let parola_rev = "";
+    
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        parola_rev += str[i];
+    }
+    
+    return parola_rev;
+}
+
+function polindromo(originale, invertito){
+    if(originale===invertito){
+        return "la parola è polindroma"
+    }
+    else{
+        return "la parola non è polindroma"
+    }
+}
+
+let parola_rev = inversione(input);
+let messaggioPalindromo = polindromo(input, parola_rev);
+
+console.log(parola_rev)
+console.log(messaggioPalindromo)
+
+
+
+
